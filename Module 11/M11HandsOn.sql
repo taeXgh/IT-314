61eca91c8e73999b5d097a231615ae1876a0373a
taeXgh <talopalo10@gmail.com>
2025-11-26 22:40:43 -0500

    progress on module 11

--- Module 11/M11HandsOn.sql
+++ Module 11/M11HandsOn.sql
@@ -1,138 +1,38 @@
-/* Assignment 3-9
-Create a PL/SQL block that retrieves and displays information for a specific project based on
-Project ID. Display the following on a single row of output: project ID, project name, number of
-pledges made, total dollars pledged, and the average pledge amount.*/
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    TYPE type_project IS RECORD(
-        project dd_project.idproj%TYPE,
-        name dd_project.projname%TYPE,
-        num_pledges NUMBER(3),
-        total_pledged NUMBER(7,2),
-        avg_pledge NUMBER(7,2)
-    );
-    rec_project type_project;
-    lv_proj_num dd_project.idproj%TYPE := 500;
-
-BEGIN
-    SELECT idproj, projname, COUNT(idpledge), SUM(pledgeamt), AVG(pledgeamt)
-    INTO rec_project
-    FROM dd_project
-    INNER JOIN dd_pledge
-        USING (idproj)
-    WHERE idproj = lv_proj_num
-    GROUP BY idproj, projname;
-    DBMS_OUTPUT.PUT_LINE('ProjectID ProjectName NumPledges TotalPledged AvgPledge');
-    DBMS_OUTPUT.PUT_LINE(rec_project.project || ' ' || 
-                         rec_project.name || ' ' || 
-                         rec_project.num_pledges || ' ' || 
-                         rec_project.total_pledged || ' ' || 
-                         rec_project.avg_pledge);
-
-END;
-/
-
-
-DROP TABLE dd_project CASCADE CONSTRAINTS;
-CREATE TABLE DD_Project (
-                   idProj number(6),
-                   Projname varchar2(60),
-                   Projstartdate DATE,
-                   Projenddate DATE,
-                   Projfundgoal number(12,2),
-                   ProjCoord varchar2(20),
-                   CONSTRAINT project_id_pk PRIMARY KEY(idProj),
-                   CONSTRAINT project_name_uk  UNIQUE (Projname)  );  
-INSERT INTO dd_project
-  VALUES (500,'Elders Assistance League', '01-SEP-2012','31-OCT-2012',15000,'Shawn Hasee');
-INSERT INTO dd_project
-  VALUES (501,'Community food pantry #21 freezer equipment', '01-OCT-2012','31-DEC-2012',65000,'Shawn Hasee');
-INSERT INTO dd_project
-  VALUES (502,'Lang Scholarship Fund', '01-JAN-2013','01-NOV-2013',100000,'Traci Brown');
-INSERT INTO dd_project
-  VALUES (503,'Animal shelter Vet Connect Program', '01-DEC-2012','30-MAR-2013',25000,'Traci Brown');
-INSERT INTO dd_project
-  VALUES (504,'Shelter Share Project 2013', '01-FEB-2013','31-JUL-2013',35000,'Traci Brown');
-drop SEQUENCE DD_PROJID_SEQ;
-COMMIT;
-
-/* Assignment 3-10
-Create a PL/SQL block to handle adding a new project. Create and use a sequence named
-DD_PROJID_SEQ to handle generating and populating the project ID. The first number issued
-by this sequence should be 530, and no caching should be used. 
-Use a record variable to handle the data to be added. 
-Data for the new row should be the following: 
-project name = HK Animal Shelter Extension, start = 1/1/2013, end = 5/31/2013, and fundraising goal = $65,000.
-Any columns not addressed in the data list are currently unknown.*/
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-
-CREATE SEQUENCE DD_PROJID_SEQ
-        INCREMENT BY 1
-        START WITH 530
-        NOCACHE;
-DECLARE
-    TYPE type_project IS RECORD(
-        project dd_project.idproj%TYPE,
-        name dd_project.projname%TYPE,
-        startdt dd_project.projstartdate%TYPE,
-        enddt dd_project.projenddate%TYPE,
-        fundgoal dd_project.projfundgoal%TYPE
-    );
-    rec_project type_project;
-    
-BEGIN
-    rec_project.project := DD_PROJID_SEQ.NEXTVAL;
-    rec_project.name := 'HK Animal Shelter Extension';
-    rec_project.startdt := TO_DATE('01-JAN-2013', 'DD-MON-YYYY');
-    rec_project.enddt := TO_DATE('31-MAY-2013', 'DD-MON-YYYY');
-    rec_project.fundgoal := 65000;
-
-    INSERT INTO DD_PROJECT (idproj, projname, projstartdate, projenddate, projfundgoal)
-    VALUES (rec_project.project, 
-    rec_project.name, 
-    rec_project.startdt, 
-    rec_project.enddt, 
-    rec_project.fundgoal);
-    COMMIT;
-
-    DBMS_OUTPUT.PUT_LINE(rec_project.project || ' | ' || rec_project.name || ' | ' ||
-        TO_CHAR(rec_project.startdt, 'MM/DD/YYYY') || ' | ' || TO_CHAR(rec_project.enddt, 'MM/DD/YYYY') || ' | ' || rec_project.fundgoal);
-END;
-/
-
-/* Assignment 3-11
-Create a PL/SQL block to retrieve and display data for all pledges made in a specified month.
-One row of output should be displayed for each pledge.
-Include the following in each row of output:
-Pledge ID, donor ID, and pledge amount
-If the pledge is being paid in a lump sum, display “Lump Sum.”
-If the pledge is being paid in monthly payments, display “Monthly - #” (with the #
-representing the number of months for payment).
-The list should be sorted to display all lump sum pledges first.
+/* Assignment 5-10
+Create a procedure named DDPROJ_SP that retrieves project information for a specific project
+based on a project ID. The procedure should have two parameters: one to accept a project ID
+value and another to return all data for the specified project. Use a record variable to have the
+procedure return all database column values for the selected project. Test the procedure with an
+anonymous block.
 */
 SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    TYPE type_pledge IS TABLE OF dd_pledge%ROWTYPE
-        INDEX BY PLS_INTEGER;
-    tbl_pledge type_pledge;
-BEGIN
-    SELECT * BULK COLLECT INTO tbl_pledge
-    FROM dd_pledge
-    WHERE TO_CHAR(pledgedate, 'MM') = '09'
-    ORDER BY paymonths;
-    FOR i IN 1..tbl_pledge.COUNT LOOP
-    CASE
-        WHEN tbl_pledge(i).paymonths = 0 THEN
-            DBMS_OUTPUT.PUT_LINE('PledgeID: ' || tbl_pledge(i).idpledge || 
-            ' | DonorID: ' || tbl_pledge(i).iddonor || 
-            ' | PledgeAmount: ' || tbl_pledge(i).pledgeamt || 
-            ' | PaymentType: Lump Sum');
-        ELSE
-            DBMS_OUTPUT.PUT_LINE('PledgeID: ' || tbl_pledge(i).idpledge || 
-            ' | DonorID: ' || tbl_pledge(i).iddonor || 
-            ' | PledgeAmount: ' || tbl_pledge(i).pledgeamt || 
-            ' | PaymentType: Monthly - ' || tbl_pledge(i).paymonths);
-    END CASE;
-    END LOOP;
-END;
-/ 
+
+
+
+
+/* Assignment 5-11
+Create a procedure named DDPAY_SP that identifies whether a donor currently has an active pledge
+with monthly payments. A donor ID is the input to the procedure. Using the donor ID, the procedure
+needs to determine whether the donor has any currently active pledges based on the status field
+and is on a monthly payment plan. If so, the procedure is to return the Boolean value TRUE.
+Otherwise, the value FALSE should be returned. Test the procedure with an anonymous block.
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
+
+
+/* Assignment 5-12
+Create a procedure named DDCKPAY_SP that confirms whether a monthly pledge payment is the
+correct amount. The procedure needs to accept two values as input: a payment amount and a
+pledge ID. Based on these inputs, the procedure should confirm that the payment is the correct
+monthly increment amount, based on pledge data in the database. If it isn’t, a custom Oracle error
+using error number 20050 and the message “Incorrect payment amount - planned payment = ??”
+should be raised. The ?? should be replaced by the correct payment amount. The database
+query in the procedure should be formulated so that no rows are returned if the pledge isn’t on
+a monthly payment plan or the pledge isn’t found. If the query returns no rows, the procedure should
+display the message “No payment information.” Test the procedure with the pledge ID 104 and the
+payment amount $25. Then test with the same pledge ID but the payment amount $20. Finally, test
+the procedure with a pledge ID for a pledge that doesn’t have monthly payments associated with it.
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
--- Module 11/M11practiceprobs.sql
+++ Module 11/M11practiceprobs.sql
@@ -1,173 +1,167 @@
---Hands On Assignment Part 1
---Assignment 3-1
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-  lv_ship_date bb_basketstatus.dtstage%TYPE;
-  lv_shipper_txt bb_basketstatus.shipper%TYPE;
-  lv_ship_num bb_basketstatus.shippingnum%TYPE;
-  lv_bask_num bb_basketstatus.idbasket%TYPE := 3;
-BEGIN
-  SELECT dtstage, shipper, shippingnum
-   INTO lv_ship_date, lv_shipper_txt, lv_ship_num
-   FROM bb_basketstatus
-   WHERE idbasket = lv_bask_num
-    AND idstage = 5;
-  DBMS_OUTPUT.PUT_LINE('Date Shipped: '||lv_ship_date);
-  DBMS_OUTPUT.PUT_LINE('Shipper: '||lv_shipper_txt);
-  DBMS_OUTPUT.PUT_LINE('Shipping #: '||lv_ship_num);
-END;
-/
---Assignment 3-2
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-  rec_ship bb_basketstatus%ROWTYPE;
-  lv_bask_num bb_basketstatus.idbasket%TYPE := 3;
-BEGIN
-  SELECT *
-   INTO rec_ship
-   FROM bb_basketstatus
-   WHERE idbasket =  lv_bask_num
-    AND idstage = 5;
-  DBMS_OUTPUT.PUT_LINE('Date Shipped: '||rec_ship.dtstage);
-  DBMS_OUTPUT.PUT_LINE('Shipper: '||rec_ship.shipper);
-  DBMS_OUTPUT.PUT_LINE('Shipping #: '||rec_ship.shippingnum);
-  DBMS_OUTPUT.PUT_LINE('Notes: '||rec_ship.notes);
-END;
-/
---Assignment 3-3
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    lv_total_num NUMBER(6,2);
-    lv_rating_txt VARCHAR2(4);
-    lv_shop_num bb_basket.idshopper%TYPE := 22;
-BEGIN
-    SELECT SUM(total)
-    INTO lv_total_num
-
-    FROM bb_basket
-    WHERE idShopper = 22
-    AND orderplaced = 1
-    GROUP BY idshopper;
-    IF lv_total_num > 200 THEN
-        lv_rating_txt := 'High';
-    ELSIF lv_total_num <200 AND lv_total_num >=100 THEN
-        lv_rating_txt :='Mid';
-    ELSE
-        lv_rating_txt := 'Low';
-
-  END IF; 
-   DBMS_OUTPUT.PUT_LINE('Shopper '||lv_shop_num||' is rated '||lv_rating_txt);
-END;
-/
-/* Assignment 3-4
-The Brewbean’s application needs a block to determine whether a customer is rated HIGH,
-MID, or LOW based on his or her total purchases. The block needs to select the total amount of
-orders for a specified customer, determine the rating, and then display the results onscreen.
-The code rates the customer HIGH if total purchases are greater than $200, MID if greater than
-$100, and LOW if $100 or lower. Use an initialized variable to provide the shopper ID.*/
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    lv_total_num NUMBER(6,2);
-    lv_rating_txt VARCHAR2(4);
-    lv_shop_num bb_basket.idshopper%TYPE := 24;
-BEGIN
-    SELECT SUM(total)
-    INTO lv_total_num
-
-    FROM bb_basket
-    WHERE idShopper = 24
-    AND orderplaced = 1
-    GROUP BY idshopper;
-    CASE
-        WHEN lv_total_num >= 200 THEN
-            lv_rating_txt := 'High';
-        WHEN lv_total_num <200 AND lv_total_num >=100 THEN
-            lv_rating_txt :='Mid';
-        ELSE
-            lv_rating_txt := 'Low';
-    END CASE;
-    DBMS_OUTPUT.PUT_LINE('Shopper '||lv_shop_num||' is rated '||lv_rating_txt);
-END;
-/
-/* Assignment 3-5
-Brewbean’s wants to include a feature in its application that calculates the total amount (quantity)
-of a specified item that can be purchased with a given amount of money. Create a block with a
-WHILE loop to increment the item’s cost until the dollar value is met. Test first with a total spending
-amount of $100 and product ID 4. Then test with an amount and a product of your choice. Use
-initialized variables to provide the total spending amount and product ID.*/
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    lv_purchaseable_qty NUMBER := 0;
-    lv_amount NUMBER;
-    lv_total_spending_amount NUMBER := 100;
-    lv_product_id bb_product.idproduct%TYPE := 4;
-    lv_product_price bb_product.price%TYPE;
-
-BEGIN
-    SELECT price
-    INTO lv_product_price
-    FROM bb_product
-    WHERE idproduct = 4;
-
-    WHILE lv_total_spending_amount >= lv_product_price LOOP
-        lv_purchaseable_qty := lv_purchaseable_qty + 1;
-        lv_total_spending_amount := lv_total_spending_amount - lv_product_price;
-        lv_amount := lv_purchaseable_qty * lv_product_price;
-    END LOOP;
-
-    DBMS_OUTPUT.PUT_LINE('qty: ' || lv_purchaseable_qty);
-    DBMS_OUTPUT.PUT_LINE('amt: ' || lv_amount);
-END;
-/
-/* Assignment 3-8
-The Brewbean’s application contains a page displaying order summary information, including
-IDBASKET, SUBTOTAL, SHIPPING, TAX, and TOTAL columns from the BB_BASKET
-table. Create a PL/SQL block with a record variable to retrieve this data and display it
-onscreen. An initialized variable should provide the IDBASKET value. Test the block using
-the basket ID 12.*/
-SELECT sysdate, 'Thalia Edwards' FROM dual;
-DECLARE
-    TYPE type_basket IS RECORD(
-        basket bb_basket.idBasket%TYPE,
-        sub bb_basket.subtotal%TYPE,
-        shipping bb_basket.shipping%TYPE,
-        tax bb_basket.tax%TYPE,
-        total bb_basket.total%TYPE
-    );
-    rec_basket type_basket;
-    lv_basket_num NUMBER(3) := 12;
-BEGIN
-    SELECT idBasket, subtotal, shipping, tax, total
-    INTO rec_basket
-    FROM bb_basket
-    WHERE idBasket = lv_basket_num;
-    DBMS_OUTPUT.PUT_LINE(rec_basket.basket);
-    DBMS_OUTPUT.PUT_LINE(rec_basket.sub);
-    DBMS_OUTPUT.PUT_LINE(rec_basket.shipping);
-    DBMS_OUTPUT.PUT_LINE(rec_basket.tax);
-    DBMS_OUTPUT.PUT_LINE(rec_basket.total);
-END;
-/
-
-/* long version
-    TYPE type_basket IS TABLE OF bb_basket%ROWTYPE
-    INDEX BY BINARY_INTEGER;
-    tbl_items type_basket;
-    lv_ind_num NUMBER(3) := 1;
-    lv_id_num bb_basket.idbasket%TYPE := 12;
-    lv_subtotal_num bb_basket.subtotal%TYPE;
-    lv_shipping_num bb_basket.shipping%TYPE;
-    lv_tax_num bb_basket.tax%TYPE;
-    lv_total_num bb_basket.total%TYPE;
-
-BEGIN
-    tbl_items(lv_ind_num).idbasket := lv_id_num;
-    tbl_items(lv_ind_num).subtotal := lv_subtotal_num;
-    tbl_items(lv_ind_num).shipping := lv_shipping_num;
-    tbl_items(lv_ind_num).tax := lv_tax_num;
-    tbl_items(lv_ind_num).total := lv_total_num;
-    DBMS_OUTPUT.PUT_LINE(lv_ind_num);
-    DBMS_OUTPUT.PUT_LINE(tbl_items(lv_ind_num).idbasket);
-END;
-/
+/* Assignment 5-5
+Create a procedure named STATUS_SHIP_SP that allows an employee in the Shipping
+Department to update an order status to add shipping information. The BB_BASKETSTATUS
+table lists events for each order so that a shopper can see the current status, date, and
+comments as each stage of the order process is finished. The IDSTAGE column of the BB_BASKETSTATUS table identifies each stage; the value 3 in this column indicates that an
+order has been shipped. The procedure should allow adding a row with an IDSTAGE of 3, date shipped, tracking
+number, and shipper. The BB_STATUS_SEQ sequence is used to provide a value for the primary
+key column. 
 */
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
+--procedure to update order status with shipping info
+CREATE OR REPLACE PROCEDURE STATUS_SHIP_SP  
+    (p_statusid IN bb_basketstatus.idstatus%TYPE,
+     p_basketid IN bb_basketstatus.idbasket%TYPE,
+     p_stageid IN bb_basketstatus.idstage%TYPE,
+     p_datestage IN bb_basketstatus.dtstage%TYPE,
+     p_notes IN bb_basketstatus.notes%TYPE,
+     p_shipper IN bb_basketstatus.shipper%TYPE,
+     p_shipNum IN bb_basketstatus.shippingnum%TYPE
+     )
+IS
+BEGIN
+    INSERT INTO bb_basketstatus
+    (idstatus, idbasket, idstage, dtstage, notes, shipper, shippingnum)
+    VALUES
+    (bb_status_seq.NEXTVAL, p_basketid, p_stageid, p_datestage, p_notes, p_shipper, p_shipNum);
+    COMMIT;
+END STATUS_SHIP_SP;
+/
+--test the procedure
+DECLARE
+    lv_statusid bb_basketstatus.idstatus%TYPE;
+    lv_basketid bb_basketstatus.idbasket%TYPE := 3;
+    lv_stageid bb_basketstatus.idstage%TYPE := 3;
+    lv_datestage bb_basketstatus.dtstage%TYPE := TO_DATE('20-FEB-12', 'DD-MON-RR');
+    lv_notes bb_basketstatus.notes%TYPE;
+    lv_shipper bb_basketstatus.shipper%TYPE;
+    lv_shipNum bb_basketstatus.shippingnum%TYPE := 'ZW2384YXK4957';
+BEGIN
+    STATUS_SHIP_SP(lv_statusid, lv_basketid, lv_stageid, lv_datestage, lv_notes, lv_shipper, lv_shipNum);  
+END;
+/
+SELECT * FROM bb_basketstatus WHERE idbasket = 3;  --check the inserted data
+delete FROM bb_basketstatus WHERE idbasket = 3 AND idstage = 3;  --cleanup test data
+
+/* Assignment 5-6
+Create a procedure that returns the most recent order status information for a specified basket.
+This procedure should determine the most recent ordering-stage entry in the BB_BASKETSTATUS
+table and return the data. Use an IF or CASE clause to return a stage description instead
+of an IDSTAGE number, which means little to shoppers. The IDSTAGE column of the
+BB_BASKETSTATUS table identifies each stage as follows:
+1—Submitted and received
+2—Confirmed, processed, sent to shipping
+3—Shipped
+4—Cancelled
+5—Back-ordered
+The procedure should accept a basket ID number and return the most recent status
+description and date the status was recorded. If no status is available for the specified basket
+ID, return a message stating that no status is available. Name the procedure STATUS_SP. Test
+the procedure twice with the basket ID 4 and then 6.
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
+CREATE OR REPLACE PROCEDURE STATUS_SP
+(p_baskketid IN bb_basketstatus.idbasket%TYPE,
+ p_status_desc OUT VARCHAR2,
+ p_status_date OUT bb_basketstatus.dtstage%TYPE)
+ IS
+ BEGIN
+    DBMS_OUTPUT.PUT_LINE('status proc called');
+    DECLARE
+        CURSOR cur_status IS
+        SELECT IDSTAGE, DTSTAGE
+        FROM BB_BASKETSTATUS
+        WHERE IDBASKET = p_baskketid
+        ORDER BY DTSTAGE DESC;
+    BEGIN
+        OPEN cur_status;
+        LOOP
+            FETCH cur_status INTO p_status_desc, p_status_date;
+            EXIT WHEN cur_status%NOTFOUND;
+            CASE p_status_desc
+                WHEN 1 THEN p_status_desc := 'Submitted and received';
+                WHEN 2 THEN p_status_desc := 'Confirmed, processed, sent to shipping';
+                WHEN 3 THEN p_status_desc := 'Shipped';
+                WHEN 4 THEN p_status_desc := 'Cancelled';
+                WHEN 5 THEN p_status_desc := 'Back-ordered';
+                ELSE p_status_desc := 'Unknown status';
+            END CASE;
+            EXIT;
+        END LOOP;
+        CLOSE cur_status;
+        END;
+END STATUS_SP;
+/
+--test the procedure
+DECLARE
+    lv_basketid bb_basketstatus.idbasket%TYPE := 6;
+    lv_status_desc VARCHAR2(100);
+    lv_status_date bb_basketstatus.dtstage%TYPE;
+BEGIN
+    STATUS_SP(lv_basketid, lv_status_desc, lv_status_date);
+    DBMS_OUTPUT.PUT_LINE(lv_status_date);
+    DBMS_OUTPUT.PUT_LINE(lv_status_desc);
+END;
+/
+/* Assignment 5-7
+Brewbean’s wants to offer an incentive of free shipping to customers who haven’t returned to
+the site since a specified date. Create a procedure named PROMO_SHIP_SP that determines
+who these customers are and then updates the BB_PROMOLIST table accordingly. The
+procedure uses the following information:
+•
+•
+•
+•
+Date cutoff—Any customers who haven’t shopped on the site since this date
+should be included as incentive participants. Use the basket creation date to
+reflect shopper activity dates.
+Month—A three-character month (such as APR) should be added to the promotion
+table to indicate which month free shipping is effective.
+Year—A four-digit year indicates the year the promotion is effective.
+promo_flag—1 represents free shipping.
+The BB_PROMOLIST table also has a USED column, which contains the default value N
+and is updated to Y when the shopper uses the promotion. Test the procedure with the cutoff
+date 15-FEB-12. Assign free shipping for the month APR and the year 2012.
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
+CREATE OR REPLACE
+PROCEDURE PROMO_SHIP_SP
+
+
+/* Assignment 5-8
+As a shopper selects products on the Brewbean’s site, a procedure is needed to add a newly
+selected item to the current shopper’s basket. Create a procedure named BASKET_ADD_SP that
+accepts a product ID, basket ID, price, quantity, size code option (1 or 2), and form code option
+(3 or 4) and uses this information to add a new item to the BB_BASKETITEM table. The table’s
+PRIMARY KEY column is generated by BB_IDBASKETITEM_SEQ. Run the procedure with the
+following values:
+•
+•
+•
+•
+•
+•
+Basket ID—14
+Product ID—8
+Price—10.80
+Quantity—1
+Size code—2
+Form code—4
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
+
+/* Assignment 5-9
+The home page of the Brewbean’s Web site has an option for members to log on with their IDs
+and passwords. Develop a procedure named MEMBER_CK_SP that accepts the ID and password
+as inputs, checks whether they make up a valid logon, and returns the member name and cookie
+value. The name should be returned as a single text string containing the first and last name.
+The head developer wants the number of parameters minimized so that the same
+parameter is used to accept the password and return the name value. Also, if the user doesn’t
+enter a valid username and password, return the value INVALID in a parameter named
+p_check. Test the procedure using a valid logon first, with the username rat55 and password
+kile. Then try it with an invalid logon by changing the username to rat.
+*/
+SELECT sysdate, 'Thalia Edwards' FROM dual;
