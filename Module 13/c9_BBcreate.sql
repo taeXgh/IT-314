fab740c0962ceef7b9ef7d7a82e7625750ddd43c
taeXgh <talopalo10@gmail.com>
2025-11-30 01:00:01 -0500

    13.2 practice probs done + updated script to be error free

--- Module 13/c9_BBcreate.sql
+++ Module 13/c9_BBcreate.sql
@@ -600,6 +600,18 @@ EXCEPTION
    p_check := 'INVALID';
 END;
 /
+CREATE OR REPLACE FUNCTION memfmt1_sf
+(p_id IN NUMBER,
+p_first IN VARCHAR2,
+p_last IN VARCHAR2)
+RETURN VARCHAR2
+IS
+lv_mem_txt VARCHAR2(35);
+BEGIN
+lv_mem_txt := 'Member '||p_id||' - '||p_first||' '||p_last;
+RETURN lv_mem_txt;
+END;
+/
 CREATE OR REPLACE PROCEDURE login_sp
  (p_user IN VARCHAR2,
   p_pass IN VARCHAR2,
